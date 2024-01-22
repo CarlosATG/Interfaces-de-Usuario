4db9cd23e4b323133866fcc777e5c509ff1654c1
CarlosATG <87503410+CarlosATG@users.noreply.github.com>
2024-01-21 20:13:01 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
 import pygame, sys
 from button import Button
-from pygame import mixer
+
 pygame.init()
 pygame.mixer.music.load("8bit.mp3")
 pygame.mixer.music.play(-1)  # The -1 means the music will loop indefinitely
+pygame.mixer.music.set_volume(0.1)
 SCREEN = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Menu")
+BG = pygame.image.load("assets/Arcoiris.png")
+Siguiente = pygame.image.load("assets/next.png").convert_alpha()
+Back= pygame.image.load("assets/back.png").convert_alpha()
+BG_Mensajes= pygame.image.load("assets/Messages.png").convert_alpha()
+font =pygame.font.Font("assets/Pixel_like.ttf",24)
+timer=pygame.time.Clock()
+snip = font.render('', True, 'black')
+speed= 3
+done= False
+class boton():
+    def __init__(self, x,y,image, scale):
+        width=image.get_width()
+        height = image.get_height()
+        self.image= pygame.transform.scale(image,(int(width*scale),int(height*scale)))
+        self.rect=self.image.get_rect()
+        self.rect.topleft=(x,y)
+        self.clicked=False
+    def draw(self):
+        pos= pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0]==1:
+                print('CLICKED')
+        SCREEN.blit(self.image,(self.rect.x,self.rect.y))
 
-BG = pygame.image.load("assets/Rainbow.png")
-
+Btn_Siguiente=boton(1200,20,Siguiente, 0.05)
+Btn_Back=boton(30,20,Back, 0.05)
+Btn_Message=boton(0, 520, BG_Mensajes, 1)
 def get_font(size): # Returns Press-Start-2P in the desired size
-    return pygame.font.Font("assets/font.ttf", size)
+    return pygame.font.Font("assets/04B_30__.TTF", size)
 
 def FirstLevel():
     blue = (0, 0, 255)  # RGB for blue
     pink = (255, 192, 203)  # RGB for pink
-    message = ""
-    font = pygame.font.Font(None, 36)
+    counter = 0
+    message=""
+    font = pygame.font.Font("assets/Pixel_like.ttf", 24)
     screen_width, screen_height = SCREEN.get_size()
-
     # Main loop for the first level
     running = True
+    Primer_Mensaje= 'Hola, vamos a empezar este nivel de la mejor manera posible...'
     while running:
+        timer.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -37,15 +64,20 @@ def FirstLevel():
         SCREEN.fill((255, 255, 255))  # Clear screen
         pygame.draw.rect(SCREEN, blue, (0, 0, screen_width // 2, screen_height))
         pygame.draw.rect(SCREEN, pink, (screen_width // 2, 0, screen_width // 2, screen_height))
-
+        Btn_Siguiente.draw()
+        Btn_Back.draw()
+        Btn_Message.draw()
         # Display the message
+        if counter < speed *len(Primer_Mensaje):
+            counter +=1
+        elif counter >=speed*len(Primer_Mensaje):
+            done = True
+        snip= font.render(Primer_Mensaje[0:counter//speed], True, 'Black')
+        SCREEN.blit(snip,(38,560))
         if message:
-            font = pygame.font.Font(None, 36)
+            font = pygame.font.Font("assets/Pixel_like.ttf", 24)
             text = font.render(message, True, (0, 0, 0))
             SCREEN.blit(text, (screen_width // 4, screen_height // 2))
-        text_box_content = "Welcome to the First Level"
-        text_box = font.render(text_box_content, True, (0, 0, 0))
-        SCREEN.blit(text_box, (500, 10))  # Adjust (10, 10) for positioning
 
         pygame.display.flip()
 def play():
@@ -115,7 +147,8 @@ def main_menu():
                                 text_input="INFO", font=get_font(75), base_color="#FFFBEF", hovering_color="White")
         QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),
                              text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-
+        pos=pygame.mouse.get_pos()
+        print(pos)
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
         for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
